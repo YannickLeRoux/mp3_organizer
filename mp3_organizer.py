05db456c0acae62ce6855ea378ce15adad9b67a9
YannickLeRoux <rasyann@hotmail.com>
2018-10-04 13:51:08 -0700

    Added a few more genres

--- mp3_organizer.py
+++ mp3_organizer.py
@@ -6,27 +6,42 @@ from mutagen.easyid3 import EasyID3
 file_path = "/Users/yannickleroux/Downloads/"
 
 nudisco = "/Users/yannickleroux/Music/MUSIC/INDIE\ ROCK\ POP\ ELECTRO/Nu\ Disco\
- 2017/".replace('\\', '')
+ 2018/".replace('\\', '')
 
 deephouse = "/Users/yannickleroux/Music/MUSIC/DANCE\ HOUSE\ ELECTRO/Deep\
- House/Deep\ House\ 2017/".replace('\\', '')
+ House/Deep\ House\ 2018/".replace('\\', '')
 
 house = "/Users/yannickleroux/Music/MUSIC/DANCE\ HOUSE\ ELECTRO/House/House\
- 2017/".replace('\\', '')
+ 2018/".replace('\\', '')
 
 techhouse = "/Users/yannickleroux/Music/MUSIC/DANCE\ HOUSE\ ELECTRO/Tech\
- House/Tech\ House\ 2017/".replace('\\', '')
+ House/Tech\ House\ 2018/".replace('\\', '')
 
 chillout = "/Users/yannickleroux/Music/MUSIC/AMBIANCE/Chill\ Out\
- 2017/".replace('\\', '')
+ 2018/".replace('\\', '')
 
 discoedits = "/Users/yannickleroux/Music/MUSIC/70\'s\ 80\'s/DISCO/DISCO\
  EDITS/".replace('\\', '')
 
-dancefloor = "/Users/yannickleroux/Music/MUSIC/DANCE\ HOUSE\ ELECTRO/2017\
+eighties = "/Users/yannickleroux/Music/MUSIC/70\'s\ 80\'s/80\'s/".replace('\\', '')
+
+disco = "/Users/yannickleroux/Music/MUSIC/70\'s\ 80\'s/DISCO/".replace('\\', '')
+
+dancefloor = "/Users/yannickleroux/Music/MUSIC/DANCE\ HOUSE\ ELECTRO/2018\
  Download/".replace('\\', '')
 
+hiphop = "/Users/yannickleroux/Music/MUSIC/RAP\ R\ N\ B/2018\ rap/".replace('\\', '')
+
 acapella = "/Users/yannickleroux/Music/MUSIC/ACAPELLAS/".replace('\\','')
+
+varietes_inter = "/Users/yannickleroux/Music/MUSIC/VARIETE\ INTERNATIONALE/2018".replace('\\','')
+
+soul = "/Users/yannickleroux/Music/MUSIC/SOUL/".replace('\\', '')
+
+jazz = "/Users/yannickleroux/Music/MUSIC/JAZZ\ ANNEES\ 30\ CROONER/".replace('\\', '')
+
+france = "/Users/yannickleroux/Music/MUSIC/VARIETE\ FRANCAISE/".replace('\\', '')
+
 
 # create a list of all files in the download folder
 files = os.listdir(file_path)
@@ -36,15 +51,43 @@ files.sort()
 def move_file(f,dst):
     '''
     helper function
-    Ask user confirmation first and then move the file to the dst path 
+    Ask user confirmation first and then move the file to the dst path
     '''
-    confirm = input('Do you want to move {0} to {1} ? (y/n) (trash) (quit) -- '.format(f,dst))
+    confirm = input('Do you want to move {0} to {1} ? (y/n) (t=trash, dh=deephouse etc) or (quit) -- '.format(f,dst))
     if confirm == 'y':
         shutil.move(src,dst+f)
-    if confirm == 'trash':
+    if confirm == 't':
         shutil.move(src,"/Users/yannickleroux/.Trash/" + f)
     if confirm == 'quit':
         exit()
+
+    if confirm == 'dh':
+        shutil.move(src, deephouse + f )
+
+    if confirm == 'h':
+        shutil.move(src, house + f )
+
+    if confirm == 'th':
+        shutil.move(src, techhouse + f )
+
+    if confirm == 'nd':
+        shutil.move(src, nudisco + f )
+
+    if confirm == 'co':
+        shutil.move(src, chillout + f )
+
+    if confirm == 'df':
+        shutil.move(src, dancefloor + f )
+
+    if confirm == 'soul':
+        shutil.move(src, soul + f )
+
+    if confirm == 'jazz':
+        shutil.move(src, jazz + f )
+
+
+    print ('**')
+    print ('**')
 
 
 
@@ -52,16 +95,16 @@ for f in files:
     if f.endswith(".mp3"): # I'm only intersted in mp3s
         src = file_path + f
 
-        # cree un dict avec les tag ID3 
+        # cree un dict avec les tag ID3
         audio = EasyID3(src)
-        
+
         # test si contient un tag genre sinon on passe
         try:
             genre = audio['genre'][0]
         except:
             continue
-        
-        if genre == 'Nu Disco':
+
+        if genre =='Indie Dance / Nu Disco' or genre == 'Nu Disco':
             move_file(f,nudisco)
 
         elif genre == 'Deep House':
@@ -69,17 +112,42 @@ for f in files:
 
         elif genre == 'House':
             move_file(f,house)
-            
+
         elif genre == 'Tech House':
             move_file(f,techhouse)
 
         elif genre == 'Chill Out':
             move_file(f,chillout)
 
-        elif genre =='Dance' or genre =='Pop':
+        elif genre in ['Dance','Future House']:
             move_file(f,dancefloor)
+
+        elif genre =='Pop':
+            move_file(f, varietes_inter)
 
         elif genre =='Acapella':
             move_file(f,acapella)
+
+        elif genre =='80\'s':
+            move_file(f,eighties)
+
+        elif genre =='Soul':
+            move_file(f,soul)
+
+        elif genre =='Disco':
+            move_file(f,disco)
+
+        elif genre =='Variété Française':
+            move_file(f,france)
+
+        elif genre =='Disco Edits':
+            move_file(f,discoedits)
+
+        elif genre =='Jazz':
+            move_file(f,jazz)
+
+        elif genre =='Hip Hop':
+            move_file(f,hiphop)
+
 
 
